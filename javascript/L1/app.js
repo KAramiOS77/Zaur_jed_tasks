ad1a68f26c41ba67e937c01ee0c850bb6cf34ad1
KAramiOS77 <zaurazammadov@gmail.com>
2022-11-04 14:49:47 +0400

    JS l1

---
+++ javascript/L1/app.js
@@ -0,0 +1,16 @@
+let en = Number(prompt("Düzbucaqlının enini daxil edin:"))
+let uzunluq = Number(prompt("Düzbucaqlının uzunluğunu daxil edin:"))
+let sahə = en * uzunluq
+alert(sahə)
+console.log(sahə)
+
+let Doğum_tarixi =Number(prompt("Doğum tarixinizi daxil edin:"))
+let hazırki_tarix= 2022
+let Yaş= hazırki_tarix- Doğum_tarixi
+alert(Yaş)
+console.log(Yaş)
+
+let km = Number(prompt("Veriləni daxil edin (km ilə):"))
+let mile = km *0.6214
+alert(mile)
+console.log(mile)
