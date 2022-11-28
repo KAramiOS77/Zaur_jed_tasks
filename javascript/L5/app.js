84021ccad645763c95af6aa8685202389248242e
Ali-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2022-11-28 15:44:42 +0400

    bug report 🐛

--- javascript/L5/app.js
+++ javascript/L5/app.js
@@ -2,7 +2,8 @@ function add(a, b, callback) {
   callback();
   return a + b;
 }
-
+//A: "" dirnaqlari yigisdiraq z/o
 add(5, 6, "alert");
 
+// A: callback-e ehtiyac yoxdur 
 function callback() {}
