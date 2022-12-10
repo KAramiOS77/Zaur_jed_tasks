2bdb63e9a908527ffc3082cb37b7f0f600eefadb
KAramiOS77 <zaurazammadov@gmail.com>
2022-12-10 18:41:05 +0400

    Task 10

---
+++ javascript/L10/app.js
@@ -0,0 +1,20 @@
+// 4 ədəd button olsun. Hansına clickləsəm bodynin rəngi o rəngdə olsun.
+
+let body = document.querySelector("body");
+let roundGrey = document.querySelector(`.grey`);
+let roundRed = document.querySelector(`.red`);
+let roundBlue = document.querySelector(`.blue`);
+let roundYellow = document.querySelector(`.yellow`);
+
+roundGrey.onclick = () => {
+  body.style.backgroundColor = "grey";
+};
+roundRed.onclick = () => {
+  body.style.backgroundColor = "red";
+};
+roundBlue.onclick = () => {
+  body.style.backgroundColor = "blue";
+};
+roundYellow.onclick = () => {
+  body.style.backgroundColor = "yellow";
+};
