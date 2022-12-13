04e76e1a445ec011491685bbae355d48a9b7e4f5
KAramiOS77 <zaurazammadov@gmail.com>
2022-12-13 22:55:23 +0400

    JS Task 12 modified

--- javascript/L12/app.js
+++ javascript/L12/app.js
@@ -51,28 +51,51 @@ const users = [
 const root = document.getElementById("root");
 
 function createElem() {
-  //A: 8? statik verme   
-  for (let i = 0; i < 8; i++) {
+  //A: 8? statik verme - arrayin uzunluguna verdim
+  for (let i = 0; i < users.length; i++) {
     const newEl = document.createElement("div");
     root.append(newEl);
-    // A: box+i-ye ehtiyac varmi?
-    newEl.className = "newelement" + " " + "box" + i; 
+    // A: box+i-ye ehtiyac varmi? bunlari her ehtimala vermisdim belke css de lazim olar sonradan silmedim
+    newEl.className = "newelement";
 
     const photo = document.createElement("img");
     photo.setAttribute("src", `./img/${users[i].img}.jpg`);
     newEl.appendChild(photo);
-    // A: image + i -> ye ehtyac varmi?
-    photo.className = "photo" + " " + "image" + i;
+    photo.className = "photo";
 
     const user = document.createElement("h1");
     user.textContent = users[i].username;
     newEl.appendChild(user);
-    user.className = "name" + " " + "heading" + i;
+    user.className = "name";
 
     const job = document.createElement("h3");
     job.textContent = users[i].job;
     newEl.appendChild(job);
-    job.className = "job" + " " + "smallheading" + i;
+    job.className = "job";
   }
 }
 createElem();
+
+function createElemV2() {
+  users.forEach(({ id, username, img, work }, index) => {
+    const newEl = document.createElement("div");
+    root.append(newEl);
+    newEl.className = "newelement";
+
+    const photo = document.createElement("img");
+    photo.setAttribute("src", `./img/${users[index].img}.jpg`);
+    newEl.appendChild(photo);
+    photo.className = "photo";
+
+    const user = document.createElement("h1");
+    user.textContent = users[index].username;
+    newEl.appendChild(user);
+    user.className = "name";
+
+    const job = document.createElement("h3");
+    job.textContent = users[index].job;
+    newEl.appendChild(job);
+    job.className = "job";
+  });
+}
+createElemV2();
