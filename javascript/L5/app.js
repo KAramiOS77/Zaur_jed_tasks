dd4b67d80f8a836c38086bef7b6fa1ffb5267017
KAramiOS77 <zaurazammadov@gmail.com>
2022-12-13 22:58:38 +0400

    JS Task 5 modified

--- javascript/L5/app.js
+++ javascript/L5/app.js
@@ -2,13 +2,8 @@
 // A: if-e ehtiyac yoxdu. Sadece, callback-e argument kimi ver bitsin))
 // A: if-i sil. callback(a+b) ver bes eder
 function add(a, b, callback) {
-  if (callback === console.log) {
-    console.log(a + b);
-  } else if (callback === alert) {
-    alert(a + b);
-  }
+  callback(a + b);
 }
-
 add(5, 6, console.log);
 
 // Task 2
